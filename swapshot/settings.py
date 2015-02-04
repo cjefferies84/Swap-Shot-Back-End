9a016848bcb408ed56a883f4f79a0ef47edec454
Chris Jefferies <cjefferies8439@gmail.com>
2015-02-04 11:37:46 -0700

    initial backend commit, included requirements file

--- swapshot/settings.py
+++ swapshot/settings.py
@@ -37,9 +37,11 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps.swaps',
+    'corsheaders',
 )
 
 MIDDLEWARE_CLASSES = (
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -47,7 +49,17 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+
 )
+
+CORS_ORIGIN_WHITELIST = (
+        'localhost:8000',
+        'localhost/',
+
+)
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'swapshot.urls'
 
